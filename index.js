8f5894a49e5c3a87b6eb6bda7fe1d0ab1994d5cf
Christian Meyer <christian.meyer@mondata.de>
2020-11-17 12:43:17 +0100

    more robust command parsing and response

--- index.js
+++ index.js
@@ -21,52 +21,75 @@ const roll = function(number) {
   return result;
 }
 
-const doRoll = function(number) {  
+const doRoll = function(number) {
   const result = roll(number);
-  
+
   const dice = number !== 1 ? "dice" : "die";
   const rollString = `rolling ${number} ${dice}: [${result.join(", ")}]`;
   const hits = result.filter(d => d > 4).length;
   const hitString = hits !== 1 ? `${hits} hits` : `${hits} hit`;
   const failures = result.filter(d => d === 1).length;
-  
+
   const strings = [rollString, hitString];
   if (failures > (result.length / 2)) {
     strings.push(`Glitched! ${failures} failures out of ${result.length}`);
     if (hits === 0) strings.push(`CRITICAL GLITCH!`)
-  };
+  }
 
   return strings.join("\n");
 }
 
+const isCommand = function(msg) {
+  const match = msg.match(/\/(r|roll)(\s*)(\d+)/i);
+  return (match && match[3]) || false;
+}
+
+const isQuery = function(msg) {
+  const match = msg.match(/(\s*)(\d+)/i);
+  return (match && match[2]) || false;
+}
+const commandError = function(input) {
+  return `Didn't understand command: "${input}". Please enter /r or /roll followed by the number of dice. Example: /r 12`
+}
+
+const inlineQueryError = function(input) {
+  return `Didn't understand query: "${input}". Please enter @srrollbot followed by the number of dice and click the suggestion. Example: @srrollbot 12`
+}
+
 const bot = new Slimbot(TELEGRAM_BOT_TOKEN);
 
-
 bot.on('message', message => {
-  const msg = message.text.toLowerCase();
-  if (msg.startsWith('/roll') || msg.startsWith('/r')) {    
-    const number = parseInt(message.text.replace('/roll', "").replace('/r', ''));
+  let response = commandError(message.text);
+  const diceNumber = isCommand(message.text);
+  if (diceNumber) {
+    const number = parseInt(diceNumber);
     if (isNaN(number)) {
       return;
     }
-    const result = doRoll(number);
-    bot.sendMessage(message.chat.id, result);
+    response = doRoll(number);
   }
+
+  bot.sendMessage(message.chat.id, response);
 });
 
 bot.on('inline_query', query => {
-  const number = parseInt(query.query);
-  if (isNaN(number)) {
-    return;
+  if (query.query === "") return;
+  let response = inlineQueryError(query.query);
+  const diceNumber = isQuery(query.query);
+  const number = parseInt(diceNumber);
+  let title = response
+  if (!isNaN(number)) {
+    title = `roll ${number}d6`;
+    const result = doRoll(number);
+    response = `@srrollbot\n${result}`
   }
-  
-  const result = doRoll(query.query);
+
   let results = JSON.stringify([{
     'id': 'roll',
     'type': 'article',
-    'title': `roll ${number}d6`,
+    'title': title,
     'input_message_content': {
-      'message_text': `@srrollbot\n${result}`,
+      'message_text': response,
       'disable_web_page_preview': true
     }
   }, ]);
